738242ff986dfda4fd242fed24d9696a79047dd3
Mohammad Abu-Shelbaia <mabushelbaia@gmail.com>
2024-02-19 14:38:17 +0200

    Implemented PID and forward movments

--- Micromouse[PlatformIO]/src/main.cpp
+++ Micromouse[PlatformIO]/src/main.cpp
@@ -1,71 +1,139 @@
 #include <Arduino.h>
 #include "Motor.h"
 
-#define EN_A 2
-#define EN_B 4
-#define M1_FORWARD 17
-#define M1_BACKWARD 16
-#define M2_FORWARD 18
-#define M2_BACKWARD 5
 
-#define ENCODERA_R 33
-#define ENCODERB_R 32
-#define ENCODERA_L 34
-#define ENCODERB_L 35
 
-volatile long encoderCount_R = 0;
-volatile long encoderCount_L = 0;
 
-void encoderA_R()
+#define EN_1 4
+#define EN_2 16
+#define M1_FORWARD 2
+#define M1_BACKWARD 0
+#define M2_FORWARD 22
+#define M2_BACKWARD 23
+
+#define LEFT_ENCODERA 17
+#define LEFT_ENCODERB 5
+#define RIGHT_ENCODERA 19
+#define RIGHT_ENCODERB 18
+
+volatile long left_encoderCount = 0;
+volatile long right_encoderCount = 0;
+
+float kp_distance = 4;
+float kd_distance = 6;
+
+
+float lastErrorDistance = 0;
+float lastErrorAngle = 0;
+
+long distanceController(int target, float kp, float kd);
+long angleController(int angle, float kp, float kd);
+void moveForward(int cells);
+
+void left_encoder()
 {
-    if (digitalRead(ENCODERA_R) > digitalRead(ENCODERB_R))
+    if (digitalRead(LEFT_ENCODERA) > digitalRead(LEFT_ENCODERB))
     {
-        encoderCount_R++;
+        left_encoderCount++;
     }
     else
     {
-        encoderCount_R--;
+        left_encoderCount--;
     }
 }
 
-void encoderA_L()
+void right_encoder()
 {
 
-    if (digitalRead(ENCODERA_L) > digitalRead(ENCODERB_L))
+    if (digitalRead(RIGHT_ENCODERA) > digitalRead(RIGHT_ENCODERB))
     {
-        encoderCount_L++;
+        right_encoderCount++;
     }
     else
     {
-        encoderCount_L--;
+        right_encoderCount--;
     }
 }
 
-Motor motor1(EN_A, M1_FORWARD, M1_BACKWARD);
-Motor motor2(EN_B, M2_FORWARD, M2_BACKWARD);
+Motor left_motor(EN_1, M1_FORWARD, M1_BACKWARD);
+Motor right_motor(EN_2, M2_FORWARD, M2_BACKWARD);
 
 void setup()
 {
     Serial.begin(115200); // Init Serial at 115200 Baud Rate.
 
-    pinMode(ENCODERA_R, INPUT);
-    pinMode(ENCODERB_R, INPUT);
-    pinMode(ENCODERA_L, INPUT);
-    pinMode(ENCODERB_L, INPUT);
+    pinMode(LEFT_ENCODERA, INPUT);
+    pinMode(LEFT_ENCODERB, INPUT);
+    pinMode(RIGHT_ENCODERA, INPUT);
+    pinMode(RIGHT_ENCODERB, INPUT);
 
-    attachInterrupt(digitalPinToInterrupt(ENCODERA_R), encoderA_R, RISING);
-    attachInterrupt(digitalPinToInterrupt(ENCODERA_L), encoderA_L, RISING);
-    motor1.setSpeed(255);
-    motor2.setSpeed(255);
+    attachInterrupt(digitalPinToInterrupt(LEFT_ENCODERA), left_encoder, RISING);
+    attachInterrupt(digitalPinToInterrupt(RIGHT_ENCODERA), right_encoder, RISING);
+
+    moveForward(10);
+
 }
-
 void loop()
 {
-    while(encoderCount_R < 1000)
+    moveForward(1);
+}
+
+long distanceController(int target, float kp, float kd)
+{
+
+    long encoderCount = (left_encoderCount + right_encoderCount) / 2;
+    long error = target - encoderCount;
+    if (abs(error) < 3)
     {
-        motor1.forward();
-        motor2.forward();
+        return 0;
     }
-    motor1.stop();
-    motor2.stop();
+    double P = kp * error;
+    double D = kd * (error - lastErrorDistance);
+
+    long output = (long) P + D;
+    lastErrorDistance = error;
+    return output;
+}
+// long angleController(int angle, float kp, float kd)
+// {
+//     long angleTicks = angle * 50;
+//     long error = angleTicks - (left_encoderCount - right_encoderCount);
+//     double P = kp * error;
+//     double D = kd * (error - lastErrorDistance);
+//     long output = (long) (P + D);
+//     lastErrorAngle = error;
+//     return output;
+// }
+
+void moveForward(int cells)
+{
+    long target = cells * 500;
+
+    long u = distanceController(target, kp_distance, kd_distance);
+    while (u != 0)
+    {
+        u = distanceController(target, kp_distance, kd_distance);
+        int speed = (int) constrain(abs(u), 0, 255);
+        Serial.println(speed);
+        right_motor.setSpeed(speed);
+        left_motor.setSpeed(speed);
+
+        if (u > 0)
+        {
+            right_motor.forward();
+            left_motor.forward();
+        }
+        else if (u < 0)
+        {
+            right_motor.backward();
+            left_motor.backward();
+        }
+        else
+        {
+            right_motor.stop();
+            left_motor.stop();
+        }
+    }
+
+
 }
---
+++ Micromouse[PlatformIO]/src/main_test.cpp
@@ -0,0 +1,56 @@
+// #include <Arduino.h>
+
+// #define IR_T1 19
+// #define IR_T2 18
+// #define IR_T3 5
+// #define IR_T4 17
+
+// #define IR_R1 16
+// #define IR_R2 4
+// #define IR_R3 0 
+// #define IR_R4 2
+
+// void setup()
+// {
+//     Serial.begin(115200); // Init Serial at 115200 Baud Rate.
+
+//     pinMode(IR_T1, OUTPUT);
+//     pinMode(IR_T2, OUTPUT);
+//     pinMode(IR_T3, OUTPUT);
+//     pinMode(IR_T4, OUTPUT);
+
+//     pinMode(IR_R1, INPUT);
+//     pinMode(IR_R2, INPUT);
+//     pinMode(IR_R3, INPUT);
+//     pinMode(IR_R4, INPUT);
+
+
+// }
+
+// void loop()
+// {
+//     digitalWrite(IR_T1, HIGH);
+//     delay(1);
+//     Serial.println(digitalRead(IR_R1));
+//     digitalWrite(IR_T1, LOW);
+//     delay(1000);
+
+//     digitalWrite(IR_T2, HIGH);
+//     delay(1);
+//     Serial.println(digitalRead(IR_R2));
+//     digitalWrite(IR_T2, LOW);
+//     delay(1000);
+
+//     digitalWrite(IR_T3, HIGH);
+//     delay(1);
+//     Serial.println(digitalRead(IR_R3));
+//     digitalWrite(IR_T3, LOW);
+//     delay(1000);
+
+
+//     digitalWrite(IR_T4, HIGH);
+//     delay(1);
+//     Serial.println(digitalRead(IR_R4));
+//     digitalWrite(IR_T4, LOW);
+//     delay(1000);
+// }
